154b228d7089e0a89b568028f3c1ecc47f3a3012
canzitis <maks.zelenin22@gmail.com>
2021-08-28 21:56:25 +0300

    added js personal data

--- Js/index.js
+++ Js/index.js
@@ -1,29 +1,62 @@
-const
-    inputSurname = document.getElementById('personal-data-input-surname').value,
-    inputTel = document.getElementById('personal-data-input-tel').value,
-    inputName = document.getElementById('personal-data-input-name').value,
-    inputMail = document.getElementById('personal-data-input-mail').value,
-    inputMiddleName = document.getElementById('personal-data-input-middle-name').value,
-    saveButtonValidate = document.getElementsByClassName('save-button'),
-    resetButtonValidate = document.getElementsByClassName('reset-button');
+const saveButtonValidate = document.getElementsByClassName('save-button'),
+    resetButtonValidate = document.getElementsByClassName('reset-button'),
+    labelInpyt = document.querySelectorAll('.label-data'),
+    inputSurname = document.getElementById('personal-data-input-surname'),
+    inputMiddleName = document.getElementById('personal-data-input-middle-name'),
+    inputTel = document.getElementById('personal-data-input-tel'),
+    inputName = document.getElementById('personal-data-input-name'),
+    inputMail = document.getElementById('personal-data-input-mail');
+
 
 
 
 
 saveButtonValidate[0].addEventListener('click', function () {
-    if (inputSurname || inputTel || inputName || inputMail || inputMiddleName === "") {
-        document.getElementById('personal-data-input-surname').className = "error"
-        document.getElementById('personal-data-input-tel').className = "error"
-        document.getElementById('personal-data-input-name').className = "error"
-        document.getElementById('personal-data-input-mail').className = "error"
-        document.getElementById('personal-data-input-middle-name').className = "error"
+    const errData = [inputSurname, inputTel, inputName, inputMail, inputMiddleName]
+    const indexLableData = []
+    const indexInputData = []
+
+
+    debugger
+    for (let i = 0; i < labelInpyt.length; i++) {
+        indexLableData.push(i)
     }
+
+    for (let b = 0; b < errData.length; b++) {
+        if (errData[b].value == "") {
+            indexInputData.push(b)
+            errData[b].className = "error"
+        }
+    }
+
+    for (let a = 0; a < indexLableData.length; a++) {
+        for (let j = 0; j < indexInputData.length; j++) {
+            if (indexLableData[a] != indexInputData[j]) {
+                labelInpyt[indexInputData[j]].style.color = "red"
+            }
+        }
+
+    }
+
+
 });
 
 resetButtonValidate[0].addEventListener('click', function () {
-    document.getElementById('personal-data-input-surname').value = '',
-        document.getElementById('personal-data-input-tel').value = '';
-    document.getElementById('personal-data-input-name').value = '';
-    document.getElementById('personal-data-input-mail').value = '';
-    document.getElementById('personal-data-input-middle-name').value = '';
+    inputSurname.value = ''
+    inputTel.value = ''
+    inputName.value = ''
+    inputMail.value = ''
+    inputMiddleName.value = ''
+
+    inputSurname.className = "personal-data-input"
+    inputMiddleName.className = "personal-data-input"
+    inputTel.className = "personal-data-input"
+    inputName.className = "personal-data-input"
+    inputMail.className = "personal-data-input"
+
+
+    for (let i = 0; i < labelInpyt.length; i++) {
+        labelInpyt[i].style.color = "#999999"
+    }
+
 });
